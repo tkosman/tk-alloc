5fcda9f2fec0da5ca85cc1d71e09d5ed65f02e76
Tymoteusz Kosman <tkosman03@gmail.com>
2024-01-11 01:17:06 +0100

    Refactor heapAllocator.c into two files.

--- heapAllocator.c
+++ heapAllocator.c
@@ -1,41 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <assert.h>
-#include <stdlib.h> 
-
-#define CHUNK_SIZE sizeof(heapChunk) //? defining size of one chunk
-#define ALIGNMENT 8 //? I'll align each chunk by 8 bytes
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1)) //? this is a macro for finding the next multiple of 8
-#define MAGIC_NUMBER 0x12345678 //? used for identifying the chunk
-
-#define heapAlloc(bytes) allocWithStats(bytes, extractFilename(__FILE__), __LINE__)
+#include "heapAllocator.h"
 
 pthread_mutex_t global_malloc_lock;
-
-//? this struct stores information about a single chunk
-typedef struct heapChunk
-{
-    int magic;
-    size_t size;
-    bool isFree;
-    struct heapChunk *next;
-
-    const char *file;
-    int line;
-
-} heapChunk;
-
-typedef struct {
-    int allocCalls;         
-    size_t totalAllocated;   
-    size_t peakMemory;
-    size_t sbrkCalls;
-    int corruptedChunks;
-    int unfreedChunks;
-} MemoryStats;
-
 MemoryStats mem_stats = {0};
 
 //? for storing the all chunks
